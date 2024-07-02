e7156926a9413d93e259bff87f91ba08698f1705
Maria Raimondi <maria_e_raimondi@estudante.sesisenai.org.br>
2024-07-02 14:35:16 -0300

    exercicio_02

---
+++ ATV WEB/lista_02/exercicio_02/index.js
@@ -0,0 +1,21 @@
+let formulario = document.getElementById("formulario")
+
+formulario.addEventListener("submit", function(evento) {
+    evento.preventDefault();
+
+    let nome =  document.getElementById("nome").value
+    let email =  document.getElementById("email").value
+
+    if (nome) {
+
+document.getElementById(" ")
+
+    }
+
+   let meuJson = {
+        nome: nome,
+        email: email
+   }
+
+   console.log(meuJson)
+})
